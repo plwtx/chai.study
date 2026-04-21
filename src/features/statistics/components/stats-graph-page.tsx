70b399881017788994d63aca81639246e57dd943
plwtx <h.lenny933@gmail.com>
2026-04-21 21:45:15 +0200

    STYLE: Dark mode update.

--- src/features/statistics/components/stats-graph-page.tsx
+++ src/features/statistics/components/stats-graph-page.tsx
@@ -1,4 +1,4 @@
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, ScrollText } from "lucide-react";
 
 const CurrentDate: React.FC = () => {
   const currentDate = new Date();
@@ -49,49 +49,50 @@ export default function DailyFocusPage() {
   return (
     <>
       <div className="font-poppins h-full w-full">
-        {/* Daily focus */}
-        <div className="relative h-dvh w-full">
-          {/* Main */}
-          <div>
-            {/* Date and switch */}
-            <section className="flex w-full items-center justify-between">
-              <CurrentDate />
-              {/* button */}
-              <div className="bg-brown-300 size-9 rounded-full" />
-            </section>
-            {/* Chart & stats view mode toggle */}
-            <section className="bg-brown-200/45 shadow-brown-600 my-3 flex w-fit gap-3 overflow-clip rounded-full p-2 shadow-inner">
-              <span className="bg-brown-600 shadow-brown-900 text-brown-50 rounded-full p-1 px-9 font-semibold shadow-xs">
+        <section>
+          {/* Date and switch view mode (future update) */}
+          <CurrentDate />
+          {/* button */}
+        </section>
+        {/* Main Statistics */}
+        <div className="mt-6 mb-12 flex flex-col">
+          {/* View mode toggle & see logs button */}
+          <section className="flex items-center justify-between">
+            <div className="bg-brown-200/45 dark:bg-dark-900 shadow-brown-600 flex h-fit w-fit gap-3 overflow-clip rounded-full p-2 shadow-inner dark:shadow-black">
+              <span className="bg-brown-600 shadow-brown-900 text-brown-50 dark:text-dark-900 dark:bg-dark-100 rounded-full p-1 px-9 font-semibold shadow-xs">
                 Daily
               </span>
               <span className="rounded-full p-1 px-9">Weekly</span>
               <span className="rounded-full p-1 px-9">Monthly</span>
               <span className="rounded-full p-1 px-9">Yearly</span>
-            </section>
-          </div>
-          {/* Main Statistics */}
-          <section className="relative flex h-96 w-full p-12">
-            <div className="flex w-1/2 flex-col items-start justify-center gap-9">
+            </div>
+            <ScrollText className="bg-brown-600 stroke-brown-50 dark:bg-dark-900 shadow-brown-600 flex size-12 gap-3 rounded-full stroke-[1px] p-3 shadow-sm dark:shadow-black" />
+          </section>
+
+          {/* Daily Stats & Graph */}
+          <section className="relative flex h-fit w-full px-9">
+            <div className="flex w-1/2 flex-col items-start justify-center gap-12">
               {/* Focused minutes */}
               <DailyFocusedTime />
               {/* Sessions completed */}
               <SessionsCompleted />
             </div>
             {/* Dividing line */}
-            <div className="bg-brown-700 mx-3 my-auto h-32 w-px" />
+            <div className="bg-brown-700 dark:bg-dark-100 mx-3 my-auto h-32 w-px" />
             {/* Weekly / Monthly / Yearly graph */}
-            <div className="h-full w-full py-3">
+            <div className="flex h-full w-full flex-col items-center justify-center py-3">
               {/* Date and switch arrows */}
-              <div className="flex w-full items-center justify-center">
+              <div className="flex w-full items-center justify-between px-12 py-2 font-light">
+                <ChevronDown className="rotate-90 stroke-[1px]" />
                 <p>
                   <span className="font-semibold">20-26</span> Apr. 2026
                 </p>
+                <ChevronDown className="rotate-270 stroke-[1px]" />
               </div>
-              <div className="bg-brown-300/45 h-full w-full" />
+              {/* Graph Chart */}
+              <div className="bg-brown-300/45 h-72 w-lg" />
             </div>
           </section>
-          {/* Absolutes */}
-          <ChevronDown className="stroke-brown-600 absolute bottom-0 left-1/2 size-9 -translate-x-1/2 animate-bounce" />
         </div>
       </div>
     </>
