d23b7550b1f6e647116d9e449a9c3c36e5db06eb
plwtx <h.lenny933@gmail.com>
2026-04-26 14:49:22 +0200

    FEAT: Overall Focus & Sessions

--- src/features/statistics/components/other-stats.tsx
+++ src/features/statistics/components/other-stats.tsx
@@ -4,17 +4,50 @@ import { Circle, ServerOff, Coffee } from "lucide-react";
 export default function OtherStat() {
   return (
     <>
-      {/* Other stats */}
-      <main className="bg-brown-400/90 corner-bevel text-brown-50 flex w-full flex-col gap-1 rounded-3xl p-12">
-        {/* Other stats */}
-        <section className="flex flex-col gap-1">
-          <h6>Focus cycles:</h6>
-          <h6>Break taken:</h6>
-          <h6>Time left till end of day (12:00 AM):</h6>
+      {/* Other Total Stats */}
+      <main className="font-poppins w-full">
+        {/* Title and divider */}
+        <section className="relative my-6 mb-12 w-full">
+          <div className="bg-brown-400 h-px w-full" />
+          <h1 className="bg-brown-500 corner-scoop dark:bg-dark-900 absolute -top-4 left-1/2 -translate-x-1/2 rounded-full px-12 py-1 text-center font-light text-white">
+            Overall focus & sessions stats
+          </h1>
+          {/* Absolute star corners */}
+          <div className="bg-brown-500 corner-scoop absolute -top-3 left-0 size-6 rounded-full" />
+          <div className="bg-brown-500 corner-scoop absolute -top-3 right-0 size-6 rounded-full" />
         </section>
-        <h6>Weekly average focus:</h6>
-        <h6>Monthly average focus:</h6>
-        <h6>Monthly best focus time & date:</h6>
+        {/* Statistics */}
+        <section className="flex flex-col gap-3">
+          {/* Overall Focus & Sessions */}
+          <section className="grid grid-cols-2 gap-6">
+            {/* Hours focused */}
+            <section className="bg-brown-200/45 shadow-brown-500 border-brown-500 relative flex aspect-square h-64 w-full flex-col items-start justify-center gap-3 overflow-clip rounded-xl border p-12 shadow-sm">
+              <h3 className="text-6xl font-semibold">9.32</h3>
+              <h4 className="font-light text-nowrap">Hours focused</h4>
+              <div className="corner-scoop border-brown-500 absolute top-1/2 -right-32 size-64 -translate-y-1/2 rotate-25 rounded-full border" />
+            </section>
+            {/* Sessions completed */}
+            <section className="bg-brown-200/45 shadow-brown-500 border-brown-500 relative flex aspect-square h-64 w-full flex-col items-start justify-center gap-3 overflow-clip rounded-xl border p-12 shadow-sm">
+              <h3 className="text-6xl font-semibold">35</h3>
+              <h4 className="font-light text-nowrap">Sessions completed</h4>
+              <div className="corner-scoop border-brown-500 absolute top-1/2 -right-32 size-64 -translate-y-1/2 rotate-25 rounded-full border" />
+            </section>
+            {/* Days focused */}
+            <section className="bg-brown-200/45 shadow-brown-500 border-brown-500 relative flex aspect-square h-64 w-full flex-col items-start justify-center gap-3 overflow-clip rounded-xl border p-12 shadow-sm">
+              <h3 className="text-6xl font-semibold">17</h3>
+              <h4 className="font-light text-nowrap">Days focused</h4>
+              <div className="corner-scoop border-brown-500 absolute top-1/2 -right-32 size-64 -translate-y-1/2 rotate-25 rounded-full border" />
+            </section>
+            {/* Average daily focused hours */}
+            <section className="bg-brown-200/45 shadow-brown-500 border-brown-500 relative flex aspect-square h-64 w-full flex-col items-start justify-center gap-3 overflow-clip rounded-xl border p-12 shadow-sm">
+              <h3 className="text-6xl font-semibold">~ 2.3</h3>
+              <h4 className="font-light text-nowrap">
+                Average daily focused hour
+              </h4>
+              <div className="corner-scoop border-brown-500 absolute top-1/2 -right-32 size-64 -translate-y-1/2 rotate-25 rounded-full border" />
+            </section>
+          </section>
+        </section>
       </main>
     </>
   );
--- src/features/statistics/index.tsx
+++ src/features/statistics/index.tsx
@@ -6,9 +6,9 @@ import DayStreak from "./components/day-streak";
 export default function Statistics() {
   return (
     <>
-      <div className="bg-brown-50 dark:bg-dark-600 text-brown-800 dark:text-dark-100 font-poppins flex h-screen w-full justify-center overflow-y-auto">
+      <div className="bg-brown-50 dark:bg-dark-600 text-brown-800 dark:text-dark-100 font-poppins flex h-screen w-full items-start justify-center overflow-y-auto">
         {/* Container */}
-        <section className="mt-55 flex h-full w-full max-w-5xl flex-col items-center justify-center">
+        <section className="mt-20 flex h-fit w-full max-w-5xl flex-col items-center">
           {/* Statistics and streak */}
           {/* <h1 className="text-3xl font-semibold">Statistics</h1> */}
           {/* General stats */}
